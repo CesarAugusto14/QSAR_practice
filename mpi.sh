e047641d16a5b0406287a20994b9ed555cfdf10a
Somayeh Seifi Shalamzari <somseifi@login-20-26.hpcc.ttu.edu>
2024-09-18 09:16:49 -0500

    Describe your changes

---
+++ mpi.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=Scafoled_splitting
+#SBATCH --output=%x.o%j
+#SBATCH --error=%x.e%j
+#SBATCH --partition nocona
+#SBATCH --nodes=1 
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=20:00:00
+##SBATCH --mem-per-cpu=3994MB  ##3.9GB, Modify based on needs   
+
+module load gcc/10.1.0 openmpi/4.0.4
+python test_scafold.py ./scaffold_data/train.csv ./scaffold_data/valid.csv 'swindow'
